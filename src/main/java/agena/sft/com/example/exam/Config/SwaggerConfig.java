5948b0b2e230d096d6cb5b637df279a456de26cd
AbidiFarah <labidifarah090@gmail.com>
2023-03-26 00:49:46 +0100

    Le 26/03/2023(Swagger)

---
+++ src/main/java/agena/sft/com/example/exam/Config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package agena.sft.com.example.exam.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(infoAPI());
+
+    }
+    public Info infoAPI() {
+        return new Info().title("KaddemProject")
+                .description("TP étude de cas")
+                .contact(contactAPI());
+
+    }
+    public Contact contactAPI() {
+        Contact contact = new Contact().name("Farah")
+                .email("farah.abidi@esprit.tn")
+                .url("https://www.linkedin.com/in/**********/");
+        return contact;
+    }
+}
